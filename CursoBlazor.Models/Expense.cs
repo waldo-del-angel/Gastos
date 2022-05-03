67014ee206212ab950f3903fb460cf2ced28fae2
waldo-del-angel <oudelangel@gmail.com>
2022-05-02 19:54:54 -0500

    Se prepara ExpenseTracker

---
+++ CursoBlazor.Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace CursoBlazor.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Cantidad { get; set; }
+        public string IdCategoria { get; set; }
+        public Categoria Categoria { get; set; }
+        public ExpenseType TipoGasto { get; set; }
+        public DateTime FechaTransaccion { get; set; }
+    }
+}
---
+++ CursoBlazor.Models/ExpenseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBlazor.Models
+{
+    public enum ExpenseType
+    {
+        Income,
+        Expense
+    }
+}
