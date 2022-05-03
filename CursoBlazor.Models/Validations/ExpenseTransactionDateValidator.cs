628f8a0c8c31ed487176c668875cf12317a01b7a
waldo-del-angel <oudelangel@gmail.com>
2022-05-03 17:46:28 -0500

    Se adiciona TabPage que contiene el Expense

---
+++ CursoBlazor.Models/Validations/ExpenseTransactionDateValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoBlazor.Models.Validations
+{
+    public class ExpenseTransactionDateValidator : ValidationAttribute
+    {
+
+    }
+}
